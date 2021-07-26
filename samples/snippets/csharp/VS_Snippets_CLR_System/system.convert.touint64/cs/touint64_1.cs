6cad3be826ec1ecfbdc183d90d97e831c665d428
WhiteBlackGoose <wbg@angouri.org>
2021-07-26 12:44:44 +0300

    Names -> Keywords
    Replacing full type names with keywords when appropriate
    
    Files affected: touint64_1.cs.
    
    See #6920

--- samples/snippets/csharp/VS_Snippets_CLR_System/system.convert.touint64/cs/touint64_1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/system.convert.touint64/cs/touint64_1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 public class Example
 {
@@ -85,12 +85,12 @@ public class Example
                            result.GetType().Name, result);
       }
       // The example displays the following output:
-      //    Converted the Char value 'a' to the UInt64 value 97.
-      //    Converted the Char value 'z' to the UInt64 value 122.
-      //    Converted the Char value '' to the UInt64 value 7.
-      //    Converted the Char value '?' to the UInt64 value 1023.
-      //    Converted the Char value '?' to the UInt64 value 32767.
-      //    Converted the Char value '?' to the UInt64 value 65534.
+      //    Converted the char value 'a' to the UInt64 value 97.
+      //    Converted the char value 'z' to the UInt64 value 122.
+      //    Converted the char value '' to the UInt64 value 7.
+      //    Converted the char value '?' to the UInt64 value 1023.
+      //    Converted the char value '?' to the UInt64 value 32767.
+      //    Converted the char value '?' to the UInt64 value 65534.
       // </Snippet3>
    }
 
@@ -189,7 +189,7 @@ public class Example
    private static void ConvertInt32()
    {
       // <Snippet7>
-      int[] numbers = { Int32.MinValue, -1, 0, 121, 340, Int32.MaxValue };
+      int[] numbers = { int.MinValue, -1, 0, 121, 340, int.MaxValue };
       ulong result;
 
       foreach (int number in numbers)
@@ -206,12 +206,12 @@ public class Example
          }
       }
       // The example displays the following output:
-      //    The Int32 value -2147483648 is outside the range of the UInt64 type.
-      //    The Int32 value -1 is outside the range of the UInt64 type.
-      //    Converted the Int32 value 0 to the UInt64 value 0.
-      //    Converted the Int32 value 121 to the UInt64 value 121.
-      //    Converted the Int32 value 340 to the UInt64 value 340.
-      //    Converted the Int32 value 2147483647 to the UInt64 value 2147483647.
+      //    The int value -2147483648 is outside the range of the UInt64 type.
+      //    The int value -1 is outside the range of the UInt64 type.
+      //    Converted the int value 0 to the UInt64 value 0.
+      //    Converted the int value 121 to the UInt64 value 121.
+      //    Converted the int value 340 to the UInt64 value 340.
+      //    Converted the int value 2147483647 to the UInt64 value 2147483647.
       // </Snippet7>
    }
 
@@ -275,16 +275,16 @@ public class Example
       }
       // The example displays the following output:
       //    Converted the Boolean value True to the UInt64 value 1.
-      //    The Int32 value -12 is outside the range of the UInt64 type.
-      //    Converted the Int32 value 163 to the UInt64 value 163.
-      //    Converted the Int32 value 935 to the UInt64 value 935.
-      //    Converted the Char value x to the UInt64 value 120.
+      //    The int value -12 is outside the range of the UInt64 type.
+      //    Converted the int value 163 to the UInt64 value 163.
+      //    Converted the int value 935 to the UInt64 value 935.
+      //    Converted the char value x to the UInt64 value 120.
       //    No conversion to a UInt64 exists for the DateTime value 5/12/2009 12:00:00 AM.
-      //    Converted the String value 104 to the UInt64 value 104.
-      //    The String value 103.0 is not in a recognizable format.
-      //    The String value -1 is outside the range of the UInt64 type.
-      //    The String value 1.00e2 is not in a recognizable format.
-      //    The String value One is not in a recognizable format.
+      //    Converted the string value 104 to the UInt64 value 104.
+      //    The string value 103.0 is not in a recognizable format.
+      //    The string value -1 is outside the range of the UInt64 type.
+      //    The string value 1.00e2 is not in a recognizable format.
+      //    The string value One is not in a recognizable format.
       //    Converted the Double value 100 to the UInt64 value 100.
       //    The Double value 1.63E+43 is outside the range of the UInt64 type.
       // </Snippet9>
@@ -353,7 +353,7 @@ public class Example
    {
       // <Snippet12>
       string[] values = { "One", "1.34e28", "-26.87", "-18", "-6.00",
-                          " 0", "137", "1601.9", Int32.MaxValue.ToString() };
+                          " 0", "137", "1601.9", int.MaxValue.ToString() };
       ulong result;
 
       foreach (string value in values)
@@ -372,15 +372,15 @@ public class Example
          }
       }
       // The example displays the following output:
-      //    The String value 'One' is not in a recognizable format.
-      //    The String value '1.34e28' is not in a recognizable format.
-      //    The String value '-26.87' is not in a recognizable format.
+      //    The string value 'One' is not in a recognizable format.
+      //    The string value '1.34e28' is not in a recognizable format.
+      //    The string value '-26.87' is not in a recognizable format.
       //    -18 is outside the range of the UInt64 type.
-      //    The String value '-6.00' is not in a recognizable format.
-      //    Converted the String value ' 0' to the UInt64 value 0.
-      //    Converted the String value '137' to the UInt64 value 137.
-      //    The String value '1601.9' is not in a recognizable format.
-      //    Converted the String value '2147483647' to the UInt64 value 2147483647.
+      //    The string value '-6.00' is not in a recognizable format.
+      //    Converted the string value ' 0' to the UInt64 value 0.
+      //    Converted the string value '137' to the UInt64 value 137.
+      //    The string value '1601.9' is not in a recognizable format.
+      //    Converted the string value '2147483647' to the UInt64 value 2147483647.
       // </Snippet12>
    }
 
