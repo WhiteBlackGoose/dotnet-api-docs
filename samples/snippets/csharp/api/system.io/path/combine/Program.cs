abfacef397584ce9702124c818bfc34d915d347c
WhiteBlackGoose <wbg@angouri.org>
2021-07-26 12:24:58 +0300

    Names -> Keywords
    Replacing full type names with keywords when appropriate        Files affected: Program.cs, Program.cs, Program.cs, Parsing.cs, Resolution.cs, options.cs, split2.cs, source.cs, source.cs, source.cs, source.cs, source.cs, source.cs, source.cs, mysample.cs.        See #6920

--- samples/snippets/csharp/api/system.io/path/combine/Program.cs
+++ samples/snippets/csharp/api/system.io/path/combine/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 class Program
@@ -7,11 +7,11 @@ class Program
     {
         Console.WriteLine("Path.Combine(String[])");
         Combine1();
-        Console.WriteLine("\nPath.Combine(String,String)");
+        Console.WriteLine("\nPath.Combine(string,String)");
         Combine2();
-        Console.WriteLine("\nPath.Combine(String,String,String)");
+        Console.WriteLine("\nPath.Combine(string,String,string)");
         Combine3();
-        Console.WriteLine("\nPath.Combine(String,String,String,String)");
+        Console.WriteLine("\nPath.Combine(string,String,string,String)");
         Combine4();
     }
 
